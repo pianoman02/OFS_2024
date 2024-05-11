e34866cd5a32a971ea3886bb21918a0295f4f73b
Jonathan ter Haar <jjonathanterhaar@gmail.com>
2024-05-11 17:37:58 +0200

    Cable class toegevoegd

--- OFS/Classes.cs
+++ OFS/Classes.cs
@@ -6,5 +6,23 @@ namespace OFS {
 		public bool fullyCharged = false;
 		public bool timeToDepart = false;
 	}
+
+    public class Cable(int capacity, Cable? upstream = null)
+    {
+        public Cable? upstream = upstream; //The cable directly upstream from this cable. Null for the cable coming directly out of the transformer.
+        public double load = 0; //current load of cable
+		public int capacity = capacity;
+
+        public List<double> changeLoads = []; //Different values for loads the cable has had.
+        public List<double> changeTimes = []; //Times where the load has changed
+
+        public void ChangeCableFlow(double powerChange, double time)
+        {
+            load += powerChange;
+            changeLoads.Add(load);
+            changeTimes.Add(time);
+            upstream?.ChangeCableFlow(powerChange, time);
+        }
+    }
 }
 
