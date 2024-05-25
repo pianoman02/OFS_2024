8003bf6bda79bb22745bb5cbb4a4089d5fc5f7be
R W <yvandoor@gmail.com>
2024-05-25 11:15:40 +0200

    Graphs of cables in python

---
+++ Statistical_analysis.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+file = open(r"Output/ELFS_summer_solar0.txt","r")
+
+cabletimes = []
+cableloads = []
+
+
+Lines = file.readlines()
+linecounter = 0
+rejectedCars = int(Lines[linecounter])
+linecounter +=1
+for cable in range(10):
+    length = int(Lines[linecounter])
+    linecounter+=1
+    cabletimes.append(np.arange(length))
+    cableloads.append(np.arange(length))
+    for i in range(length):
+        line = Lines[linecounter]
+        linecounter+=1
+        vals = line.split(";")
+        cabletimes[cable][i]= float(vals[0])
+        cableloads[cable][i] = float(vals[1])
+
+file.close()
+
+plt.figure()
+plt.plot(cabletimes[1],cableloads[1])
+plt.show()
